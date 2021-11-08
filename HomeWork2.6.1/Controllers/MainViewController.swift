b3885dfc921d57853aab83cdaa10046e57dbca79
Artur <artur.anissimov@gmail.com>
2021-11-08 22:34:06 +0200

    Code improvement Работа на ошибками. Улучшение кода.

--- HomeWork2.6.1/Controllers/MainViewController.swift
+++ HomeWork2.6.1/Controllers/MainViewController.swift
@@ -8,38 +8,22 @@
 import UIKit
 
 protocol SettingsViewControllerDelegate {
-    func setNewBackgroundColor(color: UIColor)
+    func setNewBackgroundColor(color: UIColor?)
 }
 
 class MainViewController: UIViewController {
     
-    // MARK: - Public Properties
-    var viewColor: UIColor!
-
-    // MARK: - Override Methods
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        viewColor = view.backgroundColor
-        
-    }
-    
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         guard let settingsViewController = segue.destination as? SettingsViewController else { return }
-        settingsViewController.viewBackgroundColor = viewColor
+        settingsViewController.viewColor = view.backgroundColor
         settingsViewController.delegate = self
     }
-
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        
-        view.backgroundColor = viewColor
-    }
+    
 }
 
 // MARK: - SettingsViewControllerDelegate
 extension MainViewController: SettingsViewControllerDelegate {
-    func setNewBackgroundColor(color: UIColor) {
-        viewColor = color
+    func setNewBackgroundColor(color: UIColor?) {
+        view.backgroundColor = color
     }
 }
--- HomeWork2.6.1/Controllers/SettingsViewController.swift
+++ HomeWork2.6.1/Controllers/SettingsViewController.swift
@@ -25,38 +25,41 @@ class SettingsViewController: UIViewController {
     @IBOutlet var blueTextField: UITextField!
     
     // MARK: - Public Properties
-    var viewBackgroundColor: UIColor!
+    var viewColor: UIColor!
     var delegate: SettingsViewControllerDelegate!
-    
-    // MARK: - Private Properties
-    private var red: CGFloat = 0
-    private var green: CGFloat = 0
-    private var blue: CGFloat = 0
-    private var alpha: CGFloat = 0
  
     // MARK: - Override Methods
     override func viewDidLoad() {
         super.viewDidLoad()
         colorView.layer.cornerRadius = 15
         
-        colorView.backgroundColor = viewBackgroundColor
-
-        viewBackgroundColor.getRed(&red, green: &green, blue: &blue, alpha: &alpha)
+        colorView.backgroundColor = viewColor
         
         rgbSliderSettings()
-        labelAndTextFieldSettings()
+        setValue(for: redColorLabel, greenColorLabel, blueColorLabel)
+        setValue(for: redTextField, greenTextField, blueTextField)
     }
 
     // MARK: - IB Actions
-    @IBAction func rgbSliderChanged() {
-        labelAndTextFieldSettings()
+    @IBAction func rgbSliderChanged(_ sender: UISlider) {
+        
+        switch sender {
+        case redColorSlider:
+            setValue(for: redColorLabel)
+            setValue(for: redTextField)
+        case greenColorSlider:
+            setValue(for: greenColorLabel)
+            setValue(for: greenTextField)
+        default:
+            setValue(for: blueColorLabel)
+            setValue(for: blueTextField)
+        }
+        
         changeColorInView()
     }
     
     @IBAction func doneButtonTapped() {
-        view.endEditing(true)
-        viewBackgroundColor = colorView.backgroundColor
-        delegate.setNewBackgroundColor(color: viewBackgroundColor)
+        delegate.setNewBackgroundColor(color: colorView.backgroundColor)
         dismiss(animated: true)
     }
     
@@ -67,22 +70,37 @@ class SettingsViewController: UIViewController {
                                             blue: CGFloat(blueColorSlider.value),
                                             alpha: 1)
     }
+        
+    private func setValue(for labels: UILabel...) {
+        labels.forEach { label in
+            switch label {
+            case redColorLabel: label.text = string(from: redColorSlider)
+            case greenColorLabel: label.text = string(from: greenColorSlider)
+            default: label.text = string(from: blueColorSlider)
+            }
+        }
+    }
     
-    private func labelAndTextFieldSettings() {
-        redTextField.text = String(format: "%.2f", redColorSlider.value)
-        greenTextField.text = String(format: "%.2f", greenColorSlider.value)
-        blueTextField.text = String(format: "%.2f", blueColorSlider.value)
-        
-        redColorLabel.text = String(format: "%.2f", redColorSlider.value)
-        greenColorLabel.text = String(format: "%.2f", greenColorSlider.value)
-        blueColorLabel.text = String(format: "%.2f", blueColorSlider.value)
-        
+    private func setValue(for textFields: UITextField...) {
+        textFields.forEach { textField in
+            switch textField {
+            case redTextField: textField.text = string(from: redColorSlider)
+            case greenTextField: textField.text = string(from: greenColorSlider)
+            default: textField.text = string(from: blueColorSlider)
+            }
+        }
     }
     
     private func rgbSliderSettings() {
-        redColorSlider.value = Float(red)
-        greenColorSlider.value = Float(green)
-        blueColorSlider.value = Float(blue)
+        let ciColor = CIColor(color: viewColor)
+        
+        redColorSlider.value = Float(ciColor.red)
+        greenColorSlider.value = Float(ciColor.green)
+        blueColorSlider.value = Float(ciColor.blue)
+    }
+    
+    private func string(from slider: UISlider) -> String {
+        String(format: "%.2f", slider.value)
     }
 }
 
@@ -105,23 +123,24 @@ extension SettingsViewController: UITextFieldDelegate {
     
     func textFieldDidEndEditing(_ textField: UITextField) {
         guard let value = textField.text else { return }
-        guard let number = Float(value) else { return }
-        
-        if number > 1.0 {
-            showAlert(title: "Invalid number", message: "Number must be between from 0 to 1", textField: textField)
-        } else if textField == redTextField {
-            redColorSlider.value = number
-            redColorLabel.text = String(number)
+
+        if let number = Float(value) {
+            switch textField {
+            case redTextField:
+                redColorSlider.setValue(number, animated: true)
+                setValue(for: redColorLabel)
+            case greenTextField:
+                greenColorSlider.setValue(number, animated: true)
+                setValue(for: greenColorLabel)
+            default:
+                blueColorSlider.setValue(number, animated: true)
+                setValue(for: blueColorLabel)
+            }
+            
             changeColorInView()
-        } else if textField == greenTextField {
-            greenColorSlider.value = number
-            greenColorLabel.text = String(number)
-            changeColorInView()
-        } else if textField == blueTextField {
-            blueColorSlider.value = number
-            blueColorLabel.text = String(number)
-            changeColorInView()
+            return
         }
         
+        showAlert(title: "Invalid number", message: "Number must be between from 0 to 1", textField: textField)
     }
 }
